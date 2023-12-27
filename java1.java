830d0724bfc7d9b126141b0892b5bc2d55d4f578
ankit8321 <akraj8332@gmail.com>
2023-12-28 00:19:44 +0530

    Commit2

--- java1.java
+++ java1.java
@@ -3,6 +3,8 @@ public class java1
     public static void main(String args[])
     {
         System.out.println("Hello World");
+        System.out.print("Hello\nWorld");
+        System.out.print("\nHello\nWorld");
 
     }
 }
