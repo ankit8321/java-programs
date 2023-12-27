28fda2350c6a45daf21906fd89b0b25e1626c3d6
ankit8321 <akraj8332@gmail.com>
2023-12-28 01:08:07 +0530

    commit4

--- java2.java
+++ java2.java
@@ -1,11 +1,29 @@
+
 // Variables in java
 public class java2
 {
     public static void main(String args[])
     {
-        int a=5,b=5;
-        a=a+b;
-        System.out.println("The sum of an and b is"+a);
+        // EXAMPLES
+
+        //Sum of two numbers
+        int a=5,b=5,sum;
+        sum=a+b;
+        System.out.println("The sum of an and b is"+sum);
+        
+        //Area of rectangle
+        float l=3,bb=4,area;
+        area=2*(l+bb);
+        System.out.println("The perimeter of rectangle is"+area);
+
+        //Storing a string in a variable
+
+        String name="Tony Stark was able to build an Arc Reactor in a cave.";
+        System.out.println(name);
+
+
+
+
 
     }
 
---
+++ java3.java
@@ -0,0 +1,35 @@
+/*DATA TYPES
+1.Primitive  
+.byte      1 Bytes
+.short     2 Bytes
+.char      2 Bytes
+.boolean   1 Bytes
+.int       4 Bytes
+.long      8 Bytes
+.float     4 Bytes
+.double    8 Bytes
+
+2.Non-Primitive
+.String
+.Array
+.Class
+.Object
+.Interface
+
+*/
+public class java3
+{
+    public static void main(String args[])
+    {
+        //Every data types takes its own memory 
+        byte b=8; 
+        System.out.println(b);
+        int a=128;
+        System.out.println(a);
+        char ch='A'; 
+        System.out.println(ch);
+        boolean var=true; 
+        System.out.println(var);
+        
+    }
+}
