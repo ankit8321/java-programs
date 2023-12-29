fee06e4950a894cb17d9105b856f7a425ed5285f
ankit8321 <akraj8332@gmail.com>
2023-12-29 21:11:24 +0530

    commit5

---
+++ java4.java
@@ -0,0 +1,31 @@
+// Input in java
+
+
+import java.util.*;
+public class java4
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+//  next() takes input without
+        String input=sc.next();
+        System.out.println(input);
+//  nextLine() takes input with space
+        String name=sc.nextLine();
+        System.out.println(name);
+// nextInt() takes integer input
+        int number=sc.nextInt();
+        System.out.println(number);
+//nextfloat() takes float input
+        float decimal=sc.nextFloat();
+        System.out.println(decimal);
+//nextLongBoolean()
+        boolean point=sc.nextBoolean();
+        System.out.println(point);
+
+
+
+
+
+    }
+}
