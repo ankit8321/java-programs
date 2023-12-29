94181f24ced7cfe74976289b9b51b0f0adaf14ca
ankit8321 <akraj8332@gmail.com>
2023-12-29 21:44:31 +0530

    commit6

--- java5.java
+++ java5.java
@@ -22,7 +22,7 @@ System.out.println("The sum of two number is "+sum);
 System.out.println("Enter any number");
 int c=sc.nextInt();
 System.out.println("Enter second number");
-int d=sc.nextInt();
+int d=sc.nextInt(); 
 int product=c*d;
 System.out.println("The product of two number is "+product);
 
@@ -33,6 +33,12 @@ System.out.println("Enter second number");
 int f=sc.nextInt();
 int div=e/f;
 System.out.println("The divison of two number is"+div);
+
+// Area of circle
+System.out.println("Enter the radius of circle");
+float r=sc.nextFloat();
+float area=(float)3.14*r*r;
+System.out.println("The area of circle is"+area);
 
 }
 }
