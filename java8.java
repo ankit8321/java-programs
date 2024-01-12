f9683a21c893bb5e1545efc8a285a647a9a91942
ankit8321 <akraj8332@gmail.com>
2024-01-12 22:37:26 +0530

    commit8

--- java8.java
+++ java8.java
@@ -1,3 +1,21 @@
 //Print the largest of 2
 import java.util.*;
-public class java
+public class java8
+{
+    public static void main(String args[])
+    {
+        try(Scanner sc=new Scanner(System.in))
+        {
+            System.out.println("Enter first number");
+            int a=sc.nextInt();
+            System.out.println("Enter second number");
+            int b=sc.nextInt();
+            if(a>b)
+            System.out.println(a+" is greater than "+b);
+            else
+            System.out.println(b+" is greater than "+a);
+
+
+        }
+    }
+}
