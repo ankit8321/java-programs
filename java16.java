65acd5a126be0285cbe23642ee225ecb5b48a567
ankit8321 <akaj8332@gmail.com>
2024-01-29 04:08:33 -1100

    commit14

---
+++ java16.java
@@ -0,0 +1,25 @@
+/*
+& STARTING OF LOOP
+& WHILE LOOP */
+// * printing 1 to 10"
+
+import java.util.*;
+public class java16
+{
+    public static void main(String args[])
+    {
+       try (Scanner sc=new Scanner(System.in))
+       {
+        
+        int count=1;
+        while(count<=10)
+        {
+            System.out.println(count);
+            count=count+1;
+        }
+       }
+
+       
+    }
+}
+
